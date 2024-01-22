b81aed195e2cdc5465d287b96675e3817b9b0e04
Tayba <taybarahmankhan8@gmail.com>
2024-01-21 17:36:14 -0800

    Edited commit.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:water_intake_tracker/water_intake.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<WaterIntake> waterIntakeList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,24 +22,37 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: 100,
-            width: 100,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.pink[200],
-              border: Border.all(color: Colors.pink, width: 4),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Text(
-              'Tap to add',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16),
+          InkWell(
+            onTap: () {
+              waterIntakeList.add(WaterIntake(1, DateTime.now()));
+            },
+            child: Container(
+              height: 100,
+              width: 100,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.pink[200],
+                border: Border.all(color: Colors.pink, width: 4),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Text(
+                'Tap to add',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16),
+              ),
             ),
           ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: waterIntakeList.length,
+                itemBuilder: (context, index) {
+                  return ListTile();
+                }),
+          )
         ],
       ),
     );
